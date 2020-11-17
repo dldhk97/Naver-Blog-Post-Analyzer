1eb5c263a6da544f69dfb690dcc4c8a2632eb754
dldhk97 <dldhk97@gmail.com>
2020-11-17 16:40:57 +0900

    키워드 추출 시 의존명사, 대명사, 수사 제외

--- nbpaserver_project/mainapp/api/task/keyword_task.py
+++ nbpaserver_project/mainapp/api/task/keyword_task.py
@@ -1,5 +1,7 @@
 from ..analyzer.Keyword_Extractor import analyze_keywords
 
+EXCEPT_TAG = ['NNB', 'NR', 'NP']
+
 def keyword_task(task):
     '''
     body를 받아 키워드 추출 후 문자열 리스트 반환
@@ -8,7 +10,7 @@ def keyword_task(task):
 
     keyword_list = []
     try:
-        keywords = analyze_keywords(task._dict['body'], top_k=20)
+        keywords = analyze_keywords(task._dict['body'], top_k=50)
         if not keywords:
             print('키워드 분석을 하였으나, 문서 길이가 너무 짧거나 분석할 수 없는 문서(' + task._blog_id + ', ' + task._log_no + ')')
             keywords = [['분석불가/ERR', ]]
@@ -16,9 +18,17 @@ def keyword_task(task):
         print('keyword_task analyzed done : ', str(len(keywords)))
         
         for k in keywords:
-            keyword = k[0]          # 단어/형태 모양으로 들어감
-            keyword_list.append(keyword)
-            print(keyword)
+            # 의존명사인 경우 제외함.
+            if k[0].split('/')[1] not in EXCEPT_TAG:
+                keyword = k[0]          # 단어/형태 모양으로 들어감
+                keyword_list.append(keyword)
+                print(keyword, ' appended.')
+            else:
+                print(keyword, ' excepted.')
+
+        # 추출된 키워드가 없을 경우
+        if len(keyword_list) <= 0:
+            keyword_list.append('키워드없음/EER')
         
     except Exception as e:
         print('[keyword_task]', e)
