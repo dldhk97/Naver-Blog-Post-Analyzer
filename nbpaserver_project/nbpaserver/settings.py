141f518112a37018b66778a299afce4f86e2c6f6
dldhk97 <dldhk97@gmail.com>
2020-11-19 16:37:57 +0900

    원격 접속 가능하게 세팅하였음.

--- nbpaserver_project/nbpaserver/settings.py
+++ nbpaserver_project/nbpaserver/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'u=b_m&%xt1adxugz2wa446y0q-selrg$k%uenhx48oj6hjp#2e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nbpa.ddns.net']
 
 
 # Application definition
